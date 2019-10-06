420c2402f017e350fb6d2d6724aaba9b607c2a5e
slikts <dabas@untu.ms>
2019-10-06 16:00:28 +0300

    Updates

---
+++ precache-manifest.50ea44bfa1c72882a1d44545f8269b7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71a632bffef52bd7e5b910927ae6e74",
+    "url": "/ppdcalc/index.html"
+  },
+  {
+    "revision": "a45d97dc82138e16eede",
+    "url": "/ppdcalc/static/css/main.9850932e.chunk.css"
+  },
+  {
+    "revision": "33e43f737897139b8e38",
+    "url": "/ppdcalc/static/js/2.021e1d53.chunk.js"
+  },
+  {
+    "revision": "a45d97dc82138e16eede",
+    "url": "/ppdcalc/static/js/main.ba11bfe6.chunk.js"
+  },
+  {
+    "revision": "684eacbbb9961a3b0bed",
+    "url": "/ppdcalc/static/js/runtime-main.6aa117a2.js"
+  }
+]);
